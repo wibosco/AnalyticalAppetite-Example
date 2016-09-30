45bd3053d0ac680109f2340f45a219f8d79bece6
William Boles <william@memrise.com>
2016-09-30 16:35:06 +0100

    Basic xcode project

---
+++ AnalyticalAppetite-Example/Stories/First/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  AnalyticalAppetite-Example
+//
+//  Created by William Boles on 30/09/2016.
+//  Copyright © 2016 Boles. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
